d12202ab115802b698c5a929893201aa59fd2b91
brescou <brescou.thomas@gmail.com>
2023-05-12 16:18:46 -0400

    sync prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from "./logo.svg"
+import "./App.css"
 
-function App () {
-  return (
+function App() {
+    return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +19,7 @@ function App () {
                 </a>
             </header>
         </div>
-  )
+    )
 }
 
 export default App
