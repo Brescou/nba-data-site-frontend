ca3455613437be5bac58e6a61974a251e98dd052
Brescou <thomas.rivemale@gmail.com>
2024-02-26 17:51:31 -0500

    fresh restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import React, { useState, createContext } from "react"
-import { MantineProvider, ColorSchemeProvider } from "@mantine/core"
-import { Base } from "./components/base/Base"
-import { BrowserRouter } from "react-router-dom"
+import React from "react"
 
-// import Login from "./pages/Login/Login"
-
-export const AuthContext = createContext(null)
 
 const App = () => {
     return (<h1>App</h1>)
